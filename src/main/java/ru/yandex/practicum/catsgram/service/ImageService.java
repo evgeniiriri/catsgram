6657a06deca58c05515a0509b12882f56e0085a0
Evgenii <evgenii.ridberg@yandex.ru>
2024-10-12 13:37:56 +0300

    <feat>: Added save an images. [ImageService.java]. (not tested yet).

--- src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -4,9 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.model.Image;
 import ru.yandex.practicum.catsgram.model.Post;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
@@ -19,11 +25,71 @@ public class ImageService {
 
     private final Map<Long, Image> images = new HashMap<>();
 
+    @Value("@{catsrgam.imageDirectory}")
+    private final String imageDirectory;
+    private final PostService postService;
+
+    public List<Image> saveImages(Long postId, List<MultipartFile> files) {
+        return files.stream().map(file -> saveImage(postId, file)).collect(Collectors.toList());
+    }
+
+    private Image saveImage(Long postId, MultipartFile file) {
+        Post post = postService.findById(postId).orElseThrow(
+                () -> new ConditionsNotMetException("Пост с id - " + postId + " не найден.")
+        );
+
+        Path filePath = saveFile(file, post);
+
+        Long imageId = getNextId();
+
+        Image image = new Image();
+        image.setId(imageId);
+        image.setFilePath(filePath.toString());
+        image.setPostId(postId);
+        image.setOriginalFileName(file.getOriginalFilename());
+
+        images.put(imageId, image);
+
+        return image;
+
+
+    }
+
+    private Path saveFile(MultipartFile file, Post post) {
+        try {
+            String uniqueName = String.format("%d.%s", Instant.now().toEpochMilli(),
+                    StringUtils.getFilenameExtension(file.getOriginalFilename()));
+
+            Path uploadPath = Paths.get(imageDirectory,
+                    String.valueOf(post.getAuthorId()), String.valueOf(post.getId()));
+            Path filePath = uploadPath.resolve(uniqueName);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            file.transferTo(filePath);
+            return filePath;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     // получение данных об изображениях указанного поста
     public List<Image> getPostImages(long postId) {
         return images.values()
                 .stream()
                 .filter(image -> image.getPostId() == postId)
                 .collect(Collectors.toList());
+    }
+
+    private long getNextId() {
+        long currentMaxId = images.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
     }
 }
